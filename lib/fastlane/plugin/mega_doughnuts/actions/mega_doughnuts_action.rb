2ae441c635a106b1864bb5be63d628694d00459e
Jan Trutzschler <jan.trutzschler.von.falkenstein@nl.abnamro.nl>
2020-04-17 16:17:54 +0200

    fixed typo

--- lib/fastlane/plugin/mega_doughnuts/actions/mega_doughnuts_action.rb
+++ lib/fastlane/plugin/mega_doughnuts/actions/mega_doughnuts_action.rb
@@ -9,7 +9,7 @@ module Fastlane
         UI.message("The mega_doughnuts plugin is working!")
         options = { units: "metric", APPID: "17cfcdc32adbe2e13c85d7f42967c78e" }
         weather = OpenWeather::Current.city("Amsterdam, NL", options)
-        puts  wether["weather"][0]["description"]
+        puts  weather["weather"][0]["description"]
       end
 
       def self.description
